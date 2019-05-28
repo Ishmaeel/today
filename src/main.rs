0a55a7a2560a6089f4929ee688253700134af655
Ishmaeel <Ishmaeel@users.noreply.github.com>
2019-05-28 14:36:45 +0300

    feature complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    for i in std::env::args_os().skip(1) {
+        process_arg(i);
+    }
+}
+
+fn process_arg(arg: std::ffi::OsString) {
+    let path = std::path::PathBuf::from(arg);
+
+    if path.is_file() {
+        copy_file(&path);
+    }
+}
+
+fn copy_file(source: &std::path::PathBuf) {
+    let mut target = std::path::PathBuf::from(source);
+
+    let now = chrono::Utc::now();
+
+    target.set_file_name(now.format("%Y.%m.%d.ext").to_string());
+
+    target.set_extension(source.extension().unwrap_or_default());
+
+    if target.is_file() || target.is_dir() {
+        println!("No.");
+    } else {
+        match std::fs::copy(&source, &target) {
+            Ok(_) => println!("Done."),
+            Err(err) => println!("Ouch: {}", err),
+        }
+    }
 }
